24a082c545959d5cd6cab36aa5b6f7c380172e46
Joeri Van Dooren <ure@on3ure.be>
2023-10-27 16:43:19 +0200

    add controllers

---
+++ repeater-pacontroller.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+until [ -f /sys/class/gpio/gpio16/value ]
+do
+     sleep 5
+done
+
+PTT=$(cat /sys/class/gpio/gpio5/value)
+if [ "${PTT}" == "1" ]; then
+        print "Repeater PA down"
+        echo 0 > /sys/class/gpio/gpio5/value
+else
+        echo 1 > /sys/class/gpio/gpio6/value
+fi
+
