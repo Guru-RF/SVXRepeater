88f8bbf5ccb4fdd0481f3b5e449a34fcc24d98b6
Joeri Van Dooren <ure@on3ure.be>
2023-10-27 16:45:56 +0200

    fix

--- repeater-pacontroller.sh
+++ repeater-pacontroller.sh
@@ -7,7 +7,7 @@ done
 
 PTT=$(cat /sys/class/gpio/gpio5/value)
 if [ "${PTT}" == "1" ]; then
-        print "Repeater PA down"
+        echo "Repeater PA down"
         echo 0 > /sys/class/gpio/gpio5/value
 else
         echo 1 > /sys/class/gpio/gpio5/value
